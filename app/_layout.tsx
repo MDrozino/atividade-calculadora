9db3aea308717ce621193a3b9bcfe47e2c9270eb
MDrozino <migueldrozino882@gmail.com>
2026-03-03 20:18:15 -0300

    Terceiro commit calculadora funcional

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,105 +1,272 @@
 import 'react-native-reanimated';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StyleSheet, TouchableOpacity, Text, View } from "react-native"
+import { useState } from 'react';
 
 
 export default function RootLayout() {
- 
-  return (
-    <SafeAreaView style={StyleSheet.container}>
-      <Text>Calculadora 2</Text>
-      <Text> Resultado: </Text>
 
-      <View style={styles.linha1}>
-        <TouchableOpacity style={styles.reset}>
-            <Text> C </Text>
-        </TouchableOpacity>
+    const [visor, setVisor] = useState('0');
+    const [primeiroNumero, setPrimeiroNumero] = useState<number | null>(null);
+    const [operador, setOperador] = useState<string | null>(null);
+    const [novoNumero, setNovoNumero] = useState<boolean>(false);
 
-        <TouchableOpacity style={styles.raiz}>
-            <Text> Raiz </Text>
-        </TouchableOpacity>
-      </View>
+    const numeroPressionado = (n: string) => {
+        if (novoNumero) {
+            setVisor(n);
+            setNovoNumero(false);
+        } else if (visor === '0') {
+            setVisor(n);
+        } else {
+            setVisor(visor + n)
+        }
+    };
 
-      <View style={styles.linha2}>
-        <TouchableOpacity style={styles.numero7}>
-            <Text> 7 </Text>
-        </TouchableOpacity>
+    const operadorPressionado = (o: string) => {
+        setPrimeiroNumero(parseFloat(visor.replace(',', '.')));
+        setOperador(o);
+        setNovoNumero(true);
+    }
 
-        <TouchableOpacity style={styles.numero8}>
-            <Text> 8 </Text>
-        </TouchableOpacity>
+    const igualPressionado = () => {
+        let resultado = 0;
 
-        <TouchableOpacity style={styles.numero9}>
-            <Text> 9 </Text>
-        </TouchableOpacity>
+        if (operador === '+') {
+            resultado = parseFloat(visor.replace(',', '.')) + primeiroNumero;
+        } else if (operador === '-') {
+            resultado = primeiroNumero - parseFloat(visor.replace(',', '.'));
+        } else if (operador === '*') {
+            resultado = parseFloat(visor.replace(',', '.')) * primeiroNumero;
+        } else if (operador === '/') {
+            resultado = primeiroNumero / parseFloat(visor.replace(',', '.'));
+        }
+        setVisor(resultado.toString());
+    }
 
-        <TouchableOpacity style={styles.divisao}>
-            <Text> Dividir </Text>
-        </TouchableOpacity>
-      </View>
+    const raizPressionada = () => {
+        setVisor(Math.sqrt(parseFloat(visor)).toString());
+        setNovoNumero(true);
+    };
 
-      <View style={styles.linha3}>
-        <TouchableOpacity style={styles.numero4}>
-            <Text> 4 </Text>
-        </TouchableOpacity>
+    const resetPressionado = () => {
+        setVisor('0');
+        setPrimeiroNumero(0);
+        setOperador('');
+        setNovoNumero(false);
+    };
 
-        <TouchableOpacity style={styles.numero5}>
-            <Text> 5 </Text>
-        </TouchableOpacity>
+    const virgulaPressionada = () => {
+        if (!visor.includes(',')) {
+            setVisor(visor + ',');
+        }
+    };
 
-        <TouchableOpacity style={styles.numero6}>
-            <Text> 6 </Text>
-        </TouchableOpacity>
+    return (
+        <View style={styles.container}>
+            <SafeAreaView style={styles.fundo}>
+                <Text style={{ alignItems: 'center', justifyContent: 'center', fontSize: 30 }}>Calculadora 2</Text>
 
-        <TouchableOpacity style={styles.multiplicacao}>
-            <Text> Multiplicar </Text>
-        </TouchableOpacity>
-      </View>
+                <View style={styles.visorContainer}>
+                    <Text style={{ fontSize: 50 }}>{visor}</Text>
+                </View>
 
-      <View style={styles.linha4}>
-        <TouchableOpacity style={styles.numero1}>
-            <Text> 1 </Text>
-        </TouchableOpacity>
+                <View style={styles.linha1}>
+                    <TouchableOpacity style={styles.reset} onPress={resetPressionado}>
+                        <Text> C </Text>
+                    </TouchableOpacity>
 
-        <TouchableOpacity style={styles.numero2}>
-            <Text> 2 </Text>
-        </TouchableOpacity>
+                    <Text style={styles.botaoFalso}>  </Text>
 
-        <TouchableOpacity style={styles.numero3}>
-            <Text> 3 </Text>
-        </TouchableOpacity>
+                    <Text style={styles.botaoFalso}>  </Text>
 
-        <TouchableOpacity style={styles.subtracao}>
-            <Text> Subtrair </Text>
-        </TouchableOpacity>
-      </View>
 
-      <View style={styles.linha4}>
-        <TouchableOpacity style={styles.virgula}>
-            <Text> , </Text>
-        </TouchableOpacity>
+                    <TouchableOpacity style={styles.botaoOperador} onPress={raizPressionada}>
+                        <Text> Raiz </Text>
+                    </TouchableOpacity>
+                </View>
 
-        <TouchableOpacity style={styles.numero0}>
-            <Text> 0 </Text>
-        </TouchableOpacity>
+                <View style={styles.linha2}>
+                    <TouchableOpacity style={styles.botao} onPress={() => { numeroPressionado('7') }}>
+                        <Text> 7 </Text>
+                    </TouchableOpacity>
 
-        <TouchableOpacity style={styles.igual}>
-            <Text> = </Text>
-        </TouchableOpacity>
+                    <TouchableOpacity style={styles.botao} onPress={() => { numeroPressionado('8') }}>
+                        <Text> 8 </Text>
+                    </TouchableOpacity>
 
-        <TouchableOpacity style={styles.soma}>
-            <Text> Somar </Text>
-        </TouchableOpacity>
-      </View>
+                    <TouchableOpacity style={styles.botao} onPress={() => { numeroPressionado('9') }}>
+                        <Text> 9 </Text>
+                    </TouchableOpacity>
 
-      
-    </SafeAreaView>
-  );
+                    <TouchableOpacity style={styles.botaoOperador} onPress={() => { operadorPressionado('/') }}>
+                        <Text> Dividir </Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.linha3}>
+                    <TouchableOpacity style={styles.botao} onPress={() => { numeroPressionado('4') }}>
+                        <Text> 4 </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.botao} onPress={() => { numeroPressionado('5') }}>
+                        <Text> 5 </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.botao} onPress={() => { numeroPressionado('6') }}>
+                        <Text> 6 </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.botaoOperador} onPress={() => { operadorPressionado('*') }}>
+                        <Text> Multiplicar </Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.linha4}>
+                    <TouchableOpacity style={styles.botao} onPress={() => { numeroPressionado('1') }}>
+                        <Text> 1 </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.botao} onPress={() => { numeroPressionado('2') }}>
+                        <Text> 2 </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.botao} onPress={() => { numeroPressionado('3') }}>
+                        <Text> 3 </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.botaoOperador} onPress={() => { operadorPressionado('-') }}>
+                        <Text> Subtrair </Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.linha4}>
+                    <TouchableOpacity style={styles.botao} onPress={() => { virgulaPressionada() }}>
+                        <Text> , </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.numero0} onPress={() => { numeroPressionado('0') }}>
+                        <Text> 0 </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.botaoOperador} onPress={() => { igualPressionado() }}>
+                        <Text> = </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.botaoOperador} onPress={() => { operadorPressionado('+') }}>
+                        <Text> Somar </Text>
+                    </TouchableOpacity>
+                </View>
+
+
+            </SafeAreaView>
+        </View>
+    );
 }
 
 
 const styles = StyleSheet.create({
-  container:{
-      backgroundColor: "rgba(211, 198, 198, 1)"
-  }
+    container:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        
+    },
+
+    fundo: {
+    
+        backgroundColor: "rgba(211, 198, 198, 1)",
+        borderWidth: 2,
+        borderRadius: 50,
+        padding: 10,
+        margin: 10,
+        overflow: 'hidden',
+        width: 500,
+        marginBottom: 'auto',
+    },
+
+    visorContainer: {
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+        padding: 20,
+        margin: 6,
+        height: 150,
+        backgroundColor: '#aa9b9bff',
+        borderRadius: 50,
+    },
+
+    botao: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 6,
+        borderRadius: 50,
+        height: 80,
+        backgroundColor: '#bda8a8ff',
+        padding: 10,
+
+    },
+
+    botaoFalso: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 6,
+        borderRadius: 50,
+        height: 80,
+        backgroundColor: "rgba(211, 198, 198, 1)",
+        padding: 10,
+
+    },
+
+    numero0: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 6,
+        borderRadius: 50,
+        height: 80,
+        backgroundColor: '#bda8a8ff',
+        padding: 10,
+    },
+
+    botaoOperador: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 6,
+        borderRadius: 50,
+        height: 80,
+        padding: 10,
+        backgroundColor: '#f7a72fff'
+    },
+
+    reset: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 6,
+        borderRadius: 50,
+        height: 80,
+        backgroundColor: '#fa5e5eff',
+        padding: 10,
+    },
+
+
+    linha1: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 6,
+    },
+
+    linha2: {
+        flexDirection: 'row',
+    },
+
+    linha3: {
+        flexDirection: 'row',
+    },
+
+    linha4: {
+        flexDirection: 'row',
+    },
 });
